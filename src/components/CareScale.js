95c6ec370aa9cb625fd62ac2548c390999470ca5
Hugo-Antoine <hugo.antoine@viacesi.fr>
2021-03-08 03:40:52 +0100

    first commit

---
+++ src/components/CareScale.js
@@ -0,0 +1,38 @@
+import Sun from '../assets/sun.svg'
+import Water from '../assets/water.svg'
+
+const quantityLabel = {
+	1: 'peu',
+	2: 'un peu',
+	3: 'beaucoup'
+}
+
+function CareScale({ scaleValue, careType }) {
+	const range = [1, 2, 3]
+	const scaleType =
+		careType === 'light' ? (
+			<img src={Sun} alt='sun-icon' />
+		) : (
+			<img src={Water} alt='water-icon' />
+		)
+
+	return (
+		<div
+			onClick={() =>
+				alert(
+					`Cette plante requiert ${quantityLabel[scaleValue]} ${
+						careType === 'light' ? 'de lumière' : "d'arrosage"
+					}`
+				)
+			}
+		>
+			{range.map((rangeElem) =>
+				scaleValue >= rangeElem ? (
+					<span key={rangeElem.toString()}>{scaleType}</span>
+				) : null
+			)}
+		</div>
+	)
+}
+
+export default CareScale
